db38afdd643cf0a5fa6e3aa578e2ce535561b86b
Msamuelsons <marccossamuel@hotmail.com>
2022-08-26 14:22:52 +0000

    Finalizado ✔️

--- ex010.py
+++ ex010.py
@@ -1,7 +1,7 @@
 #Faça um Programa que pergunte em que turno você estuda. 
 # Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
 #Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou
-#"Boa Noite!" ou "Valor Inválido!", conforme o caso
+#"Boa Noite!" ou "Valor Inválido!", conforme o caso.
 
 turno = input('M-matutino ou V-Vespertino ou N- Noturno. ').lower()
 
